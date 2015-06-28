70b8bf97deaae77aa6c54931e9f9ef1929ced771
Anshul Sharma <optimisticanshul@gmail.com>
2015-06-28 17:34:23 +0530

    fixed receipe

--- recipes/elastic.rb
+++ recipes/elastic.rb
@@ -13,6 +13,7 @@ template "elasticsearch.yml" do
   source 'elasticsearch.yml.erb'
   owner  'elasticsearch' and 'elasticsearch' and mode 0755
   variables(
+  	localhost_name: node[:opsworks][:instance][:hostname],
     nodes: search(:node, "name:*")
   )
   notifies :restart, 'service[elasticsearch]'
