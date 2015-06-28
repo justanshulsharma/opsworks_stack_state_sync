75274c51a5d2a2158868f94090080d780c34617f
Anshul Sharma <optimisticanshul@gmail.com>
2015-06-28 15:07:21 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,2 @@
-include_recipe 'elasticsearch::search_discovery'
-include_recipe 'elasticsearch'
 include_recipe 'opsworks_stack_state_sync::hosts'
 include_recipe 'opsworks_stack_state_sync::motd'
-include_recipe 'opsworks_stack_state_sync::elastic'
